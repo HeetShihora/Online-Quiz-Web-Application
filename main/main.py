76c223f52176bfd15052698dce8f9d323aef92e5
Heet Shihora <heetshihora04@gmail.com>
2023-04-16 14:50:43 +0530

    error page and validation added

--- main/main.py
+++ main/main.py
@@ -1,3 +1,4 @@
+from xlrd import open_workbook
 import uuid
 from flask import *
 from fileinput import filename
@@ -5,6 +6,7 @@ import pandas
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import os
+import csv
 import openpyxl
 from werkzeug.utils import secure_filename
 app = Flask(__name__)
@@ -33,23 +35,38 @@ def upload():
     if request.method == 'POST':
         f = request.files['file']
         f.save(f.filename)
-        dataframe = openpyxl.load_workbook(f.filename)
-        dataframe1 = dataframe.active
+
         id = uuid.uuid4()
         ct = 1
         li = []
-        for row in range(1, dataframe1.max_row):
+        try:
+            with open(f.filename, mode='r')as file:
 
-            for col in dataframe1.iter_cols(1, dataframe1.max_column):
-                li.append(col[row].value)
+                # reading the CSV file
+                csvFile = csv.reader(file)
 
-            cursor = mysql.connection.cursor()
-            cursor.execute(''' INSERT INTO questions VALUES(%s,%s,%s,%s,%s,%s,%s,%s)''',
-                           (id, li[0], li[1], li[2], li[3], li[4], li[5], li[6]))
-            ct += 1
-            mysql.connection.commit()
-            cursor.close()
-        return render_template('acknowledgement.html', id=id)
+                # displaying the contents of the CSV file
+                for lines in csvFile:
+                    li.append(lines)
+        except:
+            return render_template('error.html', msg='Can not open file')
+
+        data = []
+        for i in range(1, len(li)):
+            x = li[i]
+            data.append(x)
+
+        try:
+            for x in data:
+                cursor = mysql.connection.cursor()
+                cursor.execute(''' INSERT INTO questions VALUES(%s,%s,%s,%s,%s,%s,%s,%s)''',
+                               (id, x[0], x[1], x[2], x[3], x[4], x[5], x[6]))
+                ct += 1
+                mysql.connection.commit()
+                cursor.close()
+        except:
+            return render_template('error.html', msg='Could not load data from mysql server')
+    return render_template('acknowledgement.html', id=id)
 
 
 @app.route('/give')
@@ -60,8 +77,52 @@ def give():
 @app.route('/start', methods=['POST'])
 def start():
     if request.method == 'POST':
-        qid = request.form.get("quiz_id")
-        print(qid)
+        x = request.form.get("quiz_id")
+        qid = str(x)
+        qid = qid.strip()
+        try:
+            cursor = mysql.connection.cursor()
+
+            s = """select question_id,question,opt1,opt2,opt3,opt4 from questions where quiz_id=%s"""
+
+            cursor.execute(s, (qid,))
+            questions = cursor.fetchall()
+            mysql.connection.commit()
+            cursor.close()
+        except:
+            return render_template('error.html', msg='Could not load data from mysql server')
+        if len(questions) == 0:
+            return render_template('error.html', msg='No questions found for this quiz id')
+
+        return render_template('start.html', questions=questions)
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    print(request.form)
+    x = request.form.get('quiz_id')
+    ct = 0
+    qid = str(x)
+    qid = qid.strip()
+    try:
+        cursor = mysql.connection.cursor()
+
+        s = """select answer from questions where quiz_id=%s"""
+
+        cursor.execute(s, (qid,))
+        answers = cursor.fetchall()
+        mysql.connection.commit()
+    # print(answers)
+        cursor.close()
+    except:
+        return render_template('error.html', msg='Could not load data from mysql server')
+
+    if len(answers) == 0:
+        return render_template('error.html', msg='Please check your quiz id.')
+    no_que = len(answers)
+    response = []
+    correct_ans = []
+    try:
         cursor = mysql.connection.cursor()
 
         s = """select question_id,question,opt1,opt2,opt3,opt4 from questions where quiz_id=%s"""
@@ -70,35 +131,10 @@ def start():
         questions = cursor.fetchall()
         mysql.connection.commit()
         cursor.close()
-
-        return render_template('start.html', questions=questions)
-
-
-@app.route('/submit', methods=['POST'])
-def submit():
-    print(request.form)
-    qid = request.form.get('quiz_id')
-    ct = 0
-    cursor = mysql.connection.cursor()
-
-    s = """select answer from questions where quiz_id=%s"""
-
-    cursor.execute(s, (qid,))
-    answers = cursor.fetchall()
-    mysql.connection.commit()
-    print(answers)
-    cursor.close()
-    no_que = len(answers)
-    response = []
-    correct_ans = []
-    cursor = mysql.connection.cursor()
-
-    s = """select question_id,question,opt1,opt2,opt3,opt4 from questions where quiz_id=%s"""
-
-    cursor.execute(s, (qid,))
-    questions = cursor.fetchall()
-    mysql.connection.commit()
-    cursor.close()
+    except:
+        return render_template('error.html', msg='Could not load data from mysql server')
+    if len(questions) == 0:
+        return render_template('error.html', msg='Please check your quiz id.')
     for i in range(no_que):
         res = request.form.get((str(i+1)))
         res = res.strip()
@@ -111,24 +147,29 @@ def submit():
         if res == temp:
             ct += 1
     print(ct)
-    
-    que=[]
+
+    que = []
     for q in questions:
-        
-        a=q[0]
-        b=q[1].strip()
-        c=q[2].strip()
-        d=q[3].strip()
-        e=q[4].strip()
-        f=q[5].strip()
-        
-        li=[a,b,c,d,e,f]
-        
+
+        a = q[0]
+        b = q[1].strip()
+        c = q[2].strip()
+        d = q[3].strip()
+        e = q[4].strip()
+        f = q[5].strip()
+
+        li = [a, b, c, d, e, f]
+
         que.append(li)
-        
-    
-    info = zip(que, response,correct_ans)
-    return render_template('answers.html',info=info,marks=ct,total=len(que))
+    print('\n\n')
+    print(que)
+    print('\n\n')
+    print(response)
+    print('\n\n')
+    print(correct_ans)
+    print('\n\n')
+    info = zip(que, response, correct_ans)
+    return render_template('answers.html', info=info, marks=ct, total=len(que))
 
 
 app.run(host='127.0.0.1', port=3000, debug=True)
--- main/static/script.js
+++ main/static/script.js
@@ -1,4 +1,19 @@
+console.log('hello');
+document.querySelectorAll(".copy-link").forEach((copyLinkParent) => {
+    const inputField = copyLinkParent.querySelector(".copy-link-input");
+    const copyButton = copyLinkParent.querySelector(".copy-link-button");
+    const text = inputField.value;
 
+    inputField.addEventListener("focus", () => inputField.select());
+
+    copyButton.addEventListener("click", () => {
+        inputField.select();
+        navigator.clipboard.writeText(text);
+
+        inputField.value = "Copied!";
+        setTimeout(() => (inputField.value = text), 2000);
+    });
+});
 
 
 // x.addEventListener("click", () => {
